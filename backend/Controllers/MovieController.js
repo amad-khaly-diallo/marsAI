e21c5dba3c69605f7d506ea7ac8ced304c9e26ab
amad-khaly-diallo <amad-khaly.diallo@laplateforme.io>
2026-01-29 12:17:04 +0100

    config de base et connexion a la DB

---
+++ backend/Controllers/MovieController.js
@@ -0,0 +1,75 @@
+const MovieService = require('../Services/MovieService');
+const { asyncHandler } = require('../Utils/http');
+
+exports.list = asyncHandler(async (req, res) => {
+  const data = await MovieService.list();
+  res.json(data);
+});
+
+exports.get = asyncHandler(async (req, res) => {
+  const id = Number(req.params.id);
+  const data = await MovieService.getById(id);
+  res.json(data);
+});
+
+exports.create = asyncHandler(async (req, res) => {
+  const data = await MovieService.create(req.body || {});
+  res.status(201).json(data);
+});
+
+exports.update = asyncHandler(async (req, res) => {
+  const id = Number(req.params.id);
+  const data = await MovieService.update(id, req.body || {});
+  res.json(data);
+});
+
+exports.remove = asyncHandler(async (req, res) => {
+  const id = Number(req.params.id);
+  await MovieService.remove(id);
+  res.status(204).send();
+});
+
+exports.listAssets = asyncHandler(async (req, res) => {
+  const movieId = Number(req.params.movieId);
+  const data = await MovieService.listAssets(movieId);
+  res.json(data);
+});
+
+exports.listCollaborators = asyncHandler(async (req, res) => {
+  const movieId = Number(req.params.movieId);
+  const data = await MovieService.listCollaborators(movieId);
+  res.json(data);
+});
+
+exports.listTags = asyncHandler(async (req, res) => {
+  const movieId = Number(req.params.movieId);
+  const data = await MovieService.listTags(movieId);
+  res.json(data);
+});
+
+exports.addTag = asyncHandler(async (req, res) => {
+  const movieId = Number(req.params.movieId);
+  const label = (req.body && req.body.label) || null;
+  const data = await MovieService.addTag(movieId, label);
+  res.status(201).json(data);
+});
+
+exports.removeTag = asyncHandler(async (req, res) => {
+  const movieId = Number(req.params.movieId);
+  const tagId = Number(req.params.tagId);
+  const data = await MovieService.removeTag(movieId, tagId);
+  res.json(data);
+});
+
+exports.getAiDeclaration = asyncHandler(async (req, res) => {
+  const movieId = Number(req.params.movieId);
+  const data = await MovieService.getAiDeclaration(movieId);
+  res.json(data);
+});
+
+exports.upsertAiDeclaration = asyncHandler(async (req, res) => {
+  const movieId = Number(req.params.movieId);
+  const data = await MovieService.upsertAiDeclaration(movieId, req.body || {});
+  res.json(data);
+});
+
--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const HomeController = require('../Controllers/HomeController');
+const { asyncHandler } = require('../Utils/http');
+const { ping } = require('../Utils/db');
+
+const filmmakerRoutes = require('./filmmakers');
+const movieRoutes = require('./movies');
 
 const router = express.Router();
 
-// Exemple de route MVC avec un contrôleur
-router.get('/home', HomeController.index);
+router.get('/', HomeController.index);
+
+router.get(
+  '/health',
+  asyncHandler(async (req, res) => {
+    await ping();
+    res.json({
+      status: 'ok',
+      service: 'marsAI-backend',
+      timestamp: new Date().toISOString(),
+      db: 'up',
+    });
+  })
+);
+
+router.use('/filmmakers', filmmakerRoutes);
+router.use('/movies', movieRoutes);
 
 module.exports = router;
 
