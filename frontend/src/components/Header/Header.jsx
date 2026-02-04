5fc1bececdea5d01cb7201e14a130f09b7bd6122
jawad zafari <jawad.zafari@laplateforme.io>
2026-02-04 09:12:22 +0100

    A propos

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 export default function Header() {
   return (
-    <div className="header">
-      <h1>Header</h1>
+    <div className="header bg-blue-900 p-4 text-white flex gap-4 items-center left-4">
+      <a href="/" className="nav-link">Accueil</a>
+      <a href="/submission" className="nav-link">Soumission</a>
+      <a href="/admin" className="nav-link">Admin</a>
+      <a href="/Jury" className="nav-link">Jury</a>
+      <a href="/about" className="nav-link">À propos</a>
+
     </div>
   );
 }
