b0f374d43ade1d625e39e0b3d3656257da5fc99d
amad-khaly-diallo <amad-khaly.diallo@laplateforme.io>
2026-02-10 09:25:54 +0100

    export de DB

--- database/003_create_newsletters.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `newsletters` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `email` varchar(150) NOT NULL,
-  `created_at` timestamp NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
