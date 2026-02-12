da58b3279d8f22f80e9598e12c83eb0f0e91f93e
ChaymaaLabied <chaymaalabied1996@gmail.com>
2026-02-12 16:53:08 +0100

    mise en place du contexte et séparation du routage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,24 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import AProposPage from "./pages/AProposPage";
-import Participer from "./pages/Participer";
-import Partenaires from "./pages/Partenaires";
-import CGV from "./pages/CGV";
-import CGU from "./pages/CGU";
-import Admin from "./pages/Admin";
-import Jury from "./pages/Jury";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRouter from "./router/AppRouter";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import About from "./pages/Partenaires";
-import AdminVideos from "./components/admin/All-videos";
-import Contact from "./pages/Contact";
+import { AuthProvider } from "./contexts/AuthContext";
 
 export default function App() {
   return (
-    <Router>
-      <div className="flex min-h-screen flex-col">
-        <Header />
+    <AuthProvider>
+      <Router>
+        <div className="flex min-h-screen flex-col">
+          <Header />
 
-        <main className="flex-grow">
-          <Routes>
-            {/* HOME */}
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/a-propos" element={<AProposPage />} />
-            <Route path="/participer" element={<Participer />} />
-            <Route path="/partenaires" element={<Partenaires />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/jury" element={<Jury />} />
-            <Route path="/Partenaires" element={<About />} />
-            {/* alors on a partenaires et Partenaires un avec p et un avec P donc a voir  */}
-            <Route path="/cgv" element={<CGV />} />
-            <Route path="/cgu" element={<CGU />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </main>
+          <main className="flex-grow">
+            <AppRouter />
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
