13af34c82d954dc9751587ff2bd513955d4df989
ChaymaaLabied <chaymaalabied1996@gmail.com>
2026-02-12 16:53:08 +0100

    mise en place du contexte et séparation du routage

---
+++ frontend/src/router/AppRouter.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import AProposPage from "../pages/AProposPage";
+import Participer from "../pages/Participer";
+import Partenaires from "../pages/Partenaires";
+import CGV from "../pages/CGV";
+import CGU from "../pages/CGU";
+import Admin from "../pages/Admin";
+import Jury from "../pages/Jury";
+import Contact from "../pages/Contact";
+
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/a-propos" element={<AProposPage />} />
+      <Route path="/participer" element={<Participer />} />
+      <Route path="/partenaires" element={<Partenaires />} />
+      {/* <Route path="/Partenaires" element={<About />} /> */}
+      <Route path="/cgv" element={<CGV />} />
+      <Route path="/cgu" element={<CGU />} />
+      <Route path="/jury" element={<Jury />} />
+      <Route path="/contact" element={<Contact />} />
+
+      {/* admin (Admin.jsx gère déjà la vérification auth) */}
+      <Route path="/admin" element={<Admin />} />
+
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
