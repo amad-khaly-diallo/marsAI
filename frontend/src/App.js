52ddde79932b38ecc3696937fcbfbf8a2c094d8b
ChaymaaLabied <chaymaalabied1996@gmail.com>
2026-02-13 16:32:17 +0100

    petit bemol

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,25 +14,6 @@ export default function App() {
       <Router>
         <div className="flex min-h-screen flex-col">
           <Header />
-
-        <main className="flex-grow">
-          <Routes>
-            {/* HOME */}
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/films" element={<HomePhase2 />} />
-            <Route path="/a-propos" element={<AProposPage />} />
-            <Route path="/participer" element={<Participer />} />
-            <Route path="/partenaires" element={<Partenaires />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/jury" element={<Jury />} />
-            <Route path="/Partenaires" element={<About />} />
-            <Route path="/cgv" element={<CGV />} />
-            <Route path="/cgu" element={<CGU />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </main>
           <main className="flex-grow">
             <AppRouter />
           </main>
