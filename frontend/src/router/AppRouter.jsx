52cf93c95c4acd136e6c667ace67d7e8a1397f0d
ChaymaaLabied <chaymaalabied1996@gmail.com>
2026-02-13 16:36:22 +0100

    tatata

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -9,6 +9,11 @@ import CGU from "../pages/CGU";
 import Admin from "../pages/Admin";
 import Jury from "../pages/Jury";
 import Contact from "../pages/Contact";
+import { HomePhase2 } from "../components/HomePhase2";
+import { CookieBanner } from "../components/home/CookieBanner"; // Importation du composant cookie banner
+import VideoDetail from "../pages/VideoDetail";
+import About from "../pages/Partenaires";
+// import ContactPage from "../pages/Contact";
 
 export default function AppRouter() {
   return (
