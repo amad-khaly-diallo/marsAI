8ed2a642fc4fa0e5123ed7ef166ae2c33db84f24
ChaymaaLabied <chaymaalabied1996@gmail.com>
2026-02-19 09:41:45 +0100

    app.js petit modif

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
 import Header from "./components/layout/Header/Header";
 import Footer from "./components/layout/Footer/Footer";
-import About from "./pages/Partenaires";
-import Contact from "./pages/Contact";
-import { HomePhase2 } from "./components/home/HomePhase2";
 import { AuthProvider } from "./contexts";
-import VideoDetail from "./pages/VideoDetail";
-import { CookieBanner } from "./components/ui/CookieBanner"; // Importation du composant cookie banner
 
 export default function App() {
   return (
