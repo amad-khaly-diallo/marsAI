089c4e9a53ae910e434e04578751c3565f238e25
jawad zafari <jawad.zafari@laplateforme.io>
2026-02-25 09:52:27 +0100

    Affichage précis des adresses de pages dans Google Panel

--- frontend/src/components/G-Analytics/GoogleAnalytics.jsx
+++ frontend/src/components/G-Analytics/GoogleAnalytics.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga4";
 
-// code d'identification
+// Code d'identification unique
 const TRACKING_ID = "G-Q2S789KV4W"; 
 ReactGA.initialize(TRACKING_ID);
 
@@ -10,18 +10,22 @@ const GoogleAnalytics = () => {
   const location = useLocation();
 
   useEffect(() => {
-    // Envoyer les informations de la page à Google
+    // Affichage dans le panel Google
+    const pagePath = location.pathname;
+    let pageTitle = "MarsAI - " + pagePath;
+
+    // Envoyer des informations détaillées à Google
     ReactGA.send({ 
       hitType: "pageview", 
-      page: location.pathname + location.search 
+      page: pagePath + location.search,
+      title: pageTitle 
     });
-    
 
-    console.log("GA: Page View Sent for", location.pathname);
+    // rapport sur la console
+    console.log("GA: Data Sent -> Path:",pageTitle);
   }, [location]);
 
   return null;
-   // sans graphique
 };
 
 export default GoogleAnalytics;
