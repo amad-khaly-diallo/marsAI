d49e3b6d9de18c500d60fcf777c2b851cfb12586
amad-khaly-diallo <amad-khaly.diallo@laplateforme.io>
2026-02-27 13:35:39 +0100

    initilisation du cms strapi

--- strapi/src/api/page-partenaire/routes/page-partenaire.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * page-partenaire router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::page-partenaire.page-partenaire');
--- strapi/src/api/page-participer/routes/page-participer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * page-participer router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::page-participer.page-participer');
